832817d9b40e0d06bb87a47a1815ff2e0499b286
22116wl <22116wl@hvhs.school.nz>
2025-03-18 00:15:48 +0000

    game over system

--- script.js
+++ script.js
@@ -1,124 +1,34 @@
 var scene = 'menu';
+var score = 0;
+var highScore = 0;
+var wave = 1;
+
 const bulletSpeed = 10;
 const bulletFireRate = 10; //frame gap between fires, lower # = more frequent
 
 function setup() {
     cnv = new Canvas('5:7');
-
+    
     mainGunTurret = new Sprite(cnv.hw, cnv.h - 120, 120, 20, 'k');
     mainGunTurret.color = 'red';
     mainGunTurret.rotation = 90;
-
+    
     mainGunBody = new Sprite(cnv.hw, cnv.h, 150, 'k');
     mainGunBody.color = 'cyan';
-
+    
     bulletGroup = new Group()
     alienGroup = new Group()
-
+    
     bulletGroup.collides(alienGroup, function(bullet, alien){
         bullet.remove();
-        alien.health -= 20;
-        console.log(alien.health);
+
+        let damageDealt = 20;
+
+        alien.health -= damageDealt;
+        score += damageDealt / 10;
     });
 }
 
-function radToDeg(radian) {
-    return radian * (180/Math.PI);
-}
-
-function degToRad(degrees) {
-    return degrees * (Math.PI/180);
-}
-
-
-function drawButton(x, y, w, h, buttonFunction, fillColour, borderThickness) {
-    //only draw the background if a fill colour is passed in
-    if (fillColour != null) {
-        fill(fillColour);
-        strokeWeight(borderThickness);
-        rect(x, y, w, h); // draw button
-        strokeWeight(2);
-    }
-
-
-    if (mouseIsPressed == true) {
-        //check if mouse is withing bounding box of mouse
-        if (mouseX > x && mouseX < x+w && mouseY > y && mouseY < y+h) {
-            //clicked on button
-            buttonFunction();
-        }
-    }
-}
-
-
-function draw() {
-    background('#BBBBBB');
-    
-    if (scene == 'game') {
-        gameScreen();
-    } else if (scene == 'menu') {
-        menuScreen();
-    }
-}
-function menuScreen() {
-    console.log('menu');
-    textSize(50);
-
-    drawButton(cnv.hw - 300/2, cnv.hh - 100/2, 300, 100, function() {
-        scene = 'game';
-        spawnAlien();
-    }, '#DDDDDD', 3);
-
-    
-    fill('#000000');
-    textAlign(CENTER, CENTER);
-    text('Play!', cnv.hw - 300/2, cnv.h/2 - 100/2, 300, 100);
-}
-
-function gameScreen() {
-    //Position main gun turret
-    angleToMouse = Math.atan2((mouseY-mainGunBody.y), (mouseX - mainGunBody.x));
-        
-    distance = mainGunTurret.width;
-
-    mainGunTurret.rotation = radToDeg(angleToMouse);
-
-    mainGunTurret.x = mainGunBody.x + Math.cos(angleToMouse) * distance;
-    mainGunTurret.y = mainGunBody.y + Math.sin(angleToMouse) * distance;
-
-    if (kb.pressing('space') && frameCount%bulletFireRate == 0) {
-        spawnBullet();
-    }
-
-    for (let i = 0; i < bulletGroup.length; i++) {
-        let bullet = bulletGroup[i];
-        if (bullet.y < -10 || bullet.x < -10 || bullet.x > cnv.w + 10) {
-            bullet.remove();
-        }
-    }
-
-    
-    for (let i = 0; i < alienGroup.length; i++) {
-        let alien = alienGroup[i];
-
-        if (alien.health <= 0) {
-            alien.remove();
-        }
-
-
-        //Health bars
-
-        fill(230, 230, 230);
-        rect(alien.x - 20, alien.y + 35, 40, 8);
-
-        fill(0, 255, 0);
-        rect(alien.x - 20, alien.y + 35, alien.health/alien.startHealth * 40, 8);
-    }
-
-    if (frameCount % 300 == 0) {
-        spawnAlien();
-    }
-}
 
 
 function spawnBullet() {
@@ -143,11 +53,174 @@ function spawnBullet() {
 }
 
 function spawnAlien() {
-    let alien = new Sprite(cnv.hw, -10, 30, 30, "k");
+    let padding = 50;
+
+    let alien = new Sprite(random(padding, cnv.w - padding), -10, 30, 30, "k");
     alienGroup.add(alien);
 
     alien.vel.y = 2;
     
     alien.startHealth = 100;
     alien.health = alien.startHealth;
+
+    //the wave down thing uses the Y pos, offset by this random number different for each alien for randomness
+    alien.yRandomOffset = random(-2000, 2000);
+}
+
+
+
+function radToDeg(radian) {
+    return radian * (180/Math.PI);
+}
+
+function degToRad(degrees) {
+    return degrees * (Math.PI/180);
+}
+
+
+function drawButton(x, y, w, h, buttonText, buttonFunction, fillColour, borderThickness) {
+    //only draw the background if a fill colour is passed in
+    if (fillColour != null) {
+        fill(fillColour);
+        strokeWeight(borderThickness);
+        rect(x - w/2, y - h/2, w, h); // draw button
+        stroke("#FFFFFF");
+
+        noStroke();
+    }
+
+
+    if (mouseIsPressed == true) {
+        //check if mouse is withing bounding box of mouse
+        if (mouseX > x-w/2 && mouseX < x+w/2 && mouseY > y - h/2 && mouseY < y + h/2) {
+            //clicked on button
+            buttonFunction();
+        }
+    }
+
+    textSize(w/8);
+    fill('#FFFFFF');
+    textAlign(CENTER, CENTER);
+    text(buttonText, x, y);
+}
+
+
+function draw() {
+    background('#000000');
+    
+    if (scene == 'game') {
+        gameScreen();
+    } else if (scene == 'menu') {
+        menuScreen();
+    } else if (scene == 'gameOver') {
+        gameOverScreen();
+    }
+}
+
+
+function resetGame() {
+    alienGroup.removeAll();
+
+    wave = 1;
+    score = 0;
+}
+
+function menuScreen() {
+    textSize(50);
+
+    drawButton(cnv.hw, cnv.hh, 300, 100, "Play", function() {
+        scene = 'game';
+    }, '#333333', 3);
+}
+
+function gameOverScreen() {
+    textAlign(CENTER, CENTER);
+    textSize(30);
+    text('Score: ' + score, cnv.hw, cnv.h / 4);
+    textSize(15);
+    text('High Score: ' + highScore, cnv.hw, cnv.h / 4 + 50);
+
+    drawButton(cnv.hw, cnv.hh, 250, 70, "Play Again", function() {
+        resetGame();
+        scene = 'game';
+    }, '#333333', 3);
+    
+
+    drawButton(cnv.hw, cnv.hh + 70, 200, 60, "Reutrn to menu", function() {
+        resetGame();
+        scene = 'menu';
+    }, '#333333', 3);
+}
+
+function gameScreen() {
+    //Position main gun turret
+    angleToMouse = Math.atan2((mouseY-mainGunBody.y), (mouseX - mainGunBody.x));
+        
+    distance = mainGunTurret.width;
+
+    mainGunTurret.rotation = radToDeg(angleToMouse);
+
+    mainGunTurret.x = mainGunBody.x + Math.cos(angleToMouse) * distance;
+    mainGunTurret.y = mainGunBody.y + Math.sin(angleToMouse) * distance;
+
+    textSize(20);
+    fill('#FFFFFF'); 
+    textAlign(CENTER, CENTER);
+    text("Score: " + score, cnv.hw, cnv.h / 6);
+    textSize(40);
+    text("Wave: " + wave, cnv.hw, cnv.h / 8);
+
+    if (kb.pressing('space') && frameCount%bulletFireRate == 0) {
+        spawnBullet();
+    }
+
+    for (let i = 0; i < bulletGroup.length; i++) {
+        let bullet = bulletGroup[i];
+        if (bullet.y < -10 || bullet.x < -10 || bullet.x > cnv.w + 10) {
+            bullet.remove();
+        }
+    }
+
+    
+    for (let i = 0; i < alienGroup.length; i++) {
+        let alien = alienGroup[i];
+
+        if (alien.health <= 0) {
+            alien.remove();
+        }
+
+
+        //alien x pos wave down
+        let frequency = 100; // higher is less frequen
+        let amplitude = 0.5;
+        alien.vel.x = Math.sin((alien.y+alien.yRandomOffset)/frequency) * amplitude;
+
+        
+        //Health bars
+        fill(230, 230, 230);
+        rect(alien.x - 20, alien.y + 35, 40, 8);
+
+        fill(0, 255, 0);
+        rect(alien.x - 20, alien.y + 35, alien.health/alien.startHealth * 40, 8);
+
+
+        //if distance between bottom of screen and alien is less that threshold then game over
+        if ((cnv.h - alien.y) < 50) {
+            scene = 'gameOver';
+            console.log('game over!');
+
+            if (score > highScore) {
+                highScore = score;
+            }
+
+            //stop aliens
+            for (let i = 0; i < alienGroup.length; i++) {
+                alienGroup[i].vel.y = 0;
+            }
+        }
+    }
+
+    if (frameCount % 120 == 0) {
+        spawnAlien();
+    }
 }
